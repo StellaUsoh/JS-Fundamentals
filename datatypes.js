b7f041dbdd03696ed521d2e3842e6d7e8d482974
Stella Usoh <stellausohh@gmail.com>
2025-05-21 23:52:28 +0100

    Initial commit - JavaScript basics files

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+const name = "Stella";
+const age = 25;
+const isStudent = false;
+
+const address = {
+    city: "Lagos",
+    country: "Nigeria",
+};
+
+const hobbies = ["reading", "traveling", "coding"];
+
+let undefinedVariable;
+
+const nullVariable = null;
+
+console.log("Name:", name, "| Type:", typeof name);
+console.log("Age:", age, "| Type:", typeof age);
+console.log("Is Student:", isStudent, "| Type:", typeof isStudent);
+console.log("Address:", address, "| Type:", typeof address);
+console.log("Hobbies:", hobbies, "| Type:", typeof hobbies);
+console.log("Undefined Variable:", undefinedVariable, "| Type:", typeof undefinedVariable);
+console.log("Null Variable:", nullVariable, "| Type:", typeof nullVariable);
