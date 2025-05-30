d283bd602010d634edf83d382a7c4b96cdbe3763
Stella Usoh <stellausohh@gmail.com>
2025-05-30 12:49:31 +0100

    Fix 7-multi_c.js to handle negative input correctly

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,14 @@
-const arg = process.argv[2];
-const count = parseInt(arg);
+const num = Number(process.argv[2]);
 
-if (!count || count <= 0) {
-  console.log("Missing number of occurrences");
+if (!Number.isInteger(num) || num <= 0) {
+  if (num <= 0) {
+    // For zero or negative, do nothing (print nothing)
+  } else {
+    console.log("Missing number of occurrences");
+  }
 } else {
   let i = 0;
-  while (i < count) {
+  while (i < num) {
     console.log("C is fun");
     i++;
   }
